c4c0632bb804796f911b8dd89b97cab4207b3caf
Matt Anger <AngerMCS@gmail.com>
2020-10-08 21:24:19 -0700

    new base files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use tide::Request;
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    tide::log::start();
+    let mut app = tide::new();
+    app.at("/").get(|req: Request<()>| async { Ok(req) });
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
 }
