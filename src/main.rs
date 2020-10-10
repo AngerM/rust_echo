6bbf2712c8abcebf429420c1540fd724121fd5c3
Matt Anger <AngerMCS@gmail.com>
2020-10-09 18:52:41 -0700

    extract to fn

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use tide::Request;
 use tide::prelude::*; // Pulls in the json! macro.
 
+async fn echo(req: Request<()>) -> tide::Result<String> {
+    let hello = String::from("Hello");
+    Ok(hello)
+}
+
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     tide::log::start();
     let mut app = tide::new();
-    app.at("/").get(|req: Request<()>| async {
-            Ok("hello")
-        }
-    );
+    app.at("/").get(echo);
     app.listen("127.0.0.1:8080").await?;
     Ok(())
 }
