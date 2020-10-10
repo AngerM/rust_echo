eb17f926f1753da8dc6ee452da64758bf6ec52f6
Matt Anger <AngerMCS@gmail.com>
2020-10-09 18:21:13 -0700

    hello

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use tide::Request;
+use tide::prelude::*; // Pulls in the json! macro.
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     tide::log::start();
     let mut app = tide::new();
-    app.at("/").get(|req: Request<()>| async { Ok(req) });
+    app.at("/").get(|req: Request<()>| async {
+            Ok("hello")
+        }
+    );
     app.listen("127.0.0.1:8080").await?;
     Ok(())
 }
