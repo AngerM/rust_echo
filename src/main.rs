fe9f88b8095dd809605302d71d4cce6e5b1655b2
Matt Anger <AngerMCS@gmail.com>
2020-10-11 13:35:36 -0700

    basic echo

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,30 @@ use std::collections::HashMap;
 #[derive(Debug, Deserialize, Serialize)]
 struct Echo {
     method: String,
-    headers: HashMap<String, String>
+    path: String,
+    headers: HashMap<String, String>,
+    body: String
 }
 
-async fn echo(req: Request<()>) -> tide::Result<String> {
-    let echod = Echo{
+async fn echo(mut req: Request<()>) -> tide::Result<String> {
+    let mut echoed = Echo{
         method: req.method().to_string(),
-        headers: HashMap::new()
+        path: req.url().to_string(),
+        headers: HashMap::new(),
+        body: req.body_string().await.unwrap_or_else(|_|{String::from("")})
     };
-    let jsonBody = serde_json::to_string(&echod);
-    Ok(jsonBody.unwrap())
+    req.iter().for_each(|header|{
+        echoed.headers.insert(header.0.to_string(), header.1.to_string());
+    });
+    let json_body = serde_json::to_string(&echoed);
+    Ok(json_body.unwrap())
 }
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     tide::log::start();
     let mut app = tide::new();
-    app.at("/").get(echo);
+    app.at("/").all(echo);
     app.listen("127.0.0.1:8080").await?;
     Ok(())
 }
