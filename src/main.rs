8e944b8cc366a02fe9a70bfc281365ced63a7e3a
Matt Anger <AngerMCS@gmail.com>
2022-08-18 21:25:45 -0700

    switch to salvo

--- src/main.rs
+++ src/main.rs
@@ -2,58 +2,59 @@ use std::collections::HashMap;
 use std::env;
 
 use serde_json::{Map, Value};
-// Pulls in the json! macro.
-use tide::{Request, Response};
-use tide::http::StatusCode;
-use tide::prelude::*;
+use salvo::prelude::*;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Echo {
     method: String,
     path: String,
     params: HashMap<String, String>,
-    headers: HashMap<String, Vec<String>>,
+    headers: HashMap<String, String>,
     body: String,
     parsed: Map<String, Value>,
 }
 
-async fn echo(mut req: Request<()>) -> tide::Result<tide::Response> {
+#[handler]
+async fn echo(req: &mut Request, res: &mut Response) {
     // Try to parse body if Json
-    let body_str = req.body_string().await.unwrap_or(String::from(""));
+    let body_str = req.parse_body().await.unwrap_or(String::from(""));
     let parsed: Value = match serde_json::from_str(body_str.as_str()) {
         Ok(val) => val,
         Err(_) => Value::Object(Default::default()),
     };
-    let params: HashMap<String, String> = req.query().unwrap_or(HashMap::new());
     let mut echoed = Echo {
         method: req.method().to_string(),
-        path: req.url().path().to_string(),
-        params,
+        path: req.uri().path().to_string(),
+        params: HashMap::new(),
         headers: HashMap::new(),
         body: body_str,
         parsed: parsed.as_object().unwrap().clone(),
     };
-    // Swap the headers into a Vec<String>
-    req.iter().for_each(|(name, value_list)| {
+    req.queries().iter().for_each(|(k, v)| {
+        echoed.params.insert(k.to_string(), v.to_string());
+    });
+    req.headers().iter().for_each(|(name, value_list)| {
         echoed.headers.insert(
             name.to_string(),
-            value_list.iter().map(|value| value.to_string()).collect(),
+            value_list.to_str().unwrap().to_string(),
         );
     });
     let json_body = serde_json::to_string(&echoed);
-    Ok(Response::builder(StatusCode::Ok)
-        .header("Cache-Control", "no-cache")
-        .body(json_body.unwrap_or(String::from("")))
-        .build())
+    res.set_status_code(StatusCode::OK);
+    res.render(json_body.unwrap_or(String::from("")));
 }
 
-#[async_std::main]
-async fn main() -> tide::Result<()> {
-    tide::log::start();
-    let mut app = tide::new();
-    app.at("/").all(echo);
-    app.at("/*").all(echo);
+#[tokio::main]
+async fn main() {
     let port = env::var("PORT").unwrap_or(String::from("8080"));
-    app.listen(format!("0.0.0.0:{}", port)).await?;
-    Ok(())
+    let router = Router::new()
+        .path("<*>")
+        .handle(echo);
+    let addr = format!("0.0.0.0:{}", port);
+    Server::new(
+        TcpListener::bind(
+            addr.as_str()
+        )
+    ).serve(router).await;
 }
